cf8866e01a8e11fa34832678f2b6367f1d451e40
Shambhunath Gupta <65074354+Sham-23@users.noreply.github.com>
2021-03-11 22:11:46 +0530

    two_list_into_dict

---
+++ convert_two_list_into_dictionary.py
@@ -0,0 +1,6 @@
+def conversion():
+    keys=[1,2,3,4,5]
+    values=['David','Roman','Raj','Khusi','Moxly']
+    result=dict(zip(keys,values)) #first keys and values zi together and then convert into dictionary 
+    print(result)
+conversion()
